95f54f30aaca5160b890c64607d39b556116847b
Xforeverm <1296754116@qq.com>
2023-06-12 21:00:35 +0800

    fix prod

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -3,13 +3,18 @@ import Cookies from 'js-cookie'
 const TokenKey = 'vue_admin_template_token'
 
 export function getToken() {
-  return Cookies.get(TokenKey)
+  // 由于打包 导致Cookies无效 获取不到token 使用localstorage
+  // return Cookies.get(TokenKey)
+  return localStorage.getItem(TokenKey)
 }
 
 export function setToken(token) {
-  return Cookies.set(TokenKey, token)
+  // 由于打包 导致Cookies无效 获取不到token 使用localstorage
+  // return Cookies.set(TokenKey, token)
+  return localStorage.setItem(TokenKey, token)
 }
 
 export function removeToken() {
-  return Cookies.remove(TokenKey)
+  // 由于打包 导致Cookies无效 获取不到token 使用localstorage
+  return localStorage.removeItem(TokenKey)
 }
